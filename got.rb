eb78d574ee540a0f985473cf6794e730a718a5d6
skanehira <sho19921005@gmail.com>
2018-11-19 11:21:10 +0900

    Update Version 1.0.3

--- got.rb
+++ got.rb
@@ -2,10 +2,10 @@ require "formula"
 
 class Got < Formula
   homepage "https://github.com/skanehira/got"
-  url "https://github.com/skanehira/got/releases/download/v1.0.2/MacOS.zip"
-  sha256 "9e8b503e8093caed5f3330d697d6f92cc2d603e9bb6875af1d8f46ff8fc38307"
+  url "https://github.com/skanehira/got/releases/download/v1.0.3/MacOS.zip"
+  sha256 "39112cb56582018e4262e84bb1432935f20392373d6da81e6cde1861fc56ef76"
   head "https://github.com/skanehira/got.git"
-  version "v1.0.2"
+  version "v1.0.3"
 
   def install
     bin.install "Got"
